1d2d3b3b8a9351ce9d048ae9a32d3fd9bdf8e9d0
Donething <donething.net@gmail.com>
2023-02-19 14:48:58 +0800

    增加判断是否已发送过该番号

--- src/funcs/tgbot.tsx
+++ src/funcs/tgbot.tsx
@@ -1,6 +1,6 @@
 import React from "react"
-import {Divider, Stack} from "@mui/material"
-import {DoTextFieldBtn, useSharedSnackbar} from "do-comps"
+import {Button, Checkbox, Divider, FormControlLabel, Stack, TextField} from "@mui/material"
+import {useSharedSnackbar} from "do-comps"
 import {request} from "do-utils"
 import {JResult} from "../comm/typedef"
 
@@ -9,28 +9,56 @@ const TAG = "[TGBot]"
 
 // 番号面板
 const FanHao = React.memo((): JSX.Element => {
+  const [fh, setFh] = React.useState("")
+  const [extra, setExtra] = React.useState("")
+  const [still, setStill] = React.useState(false)
+  const [working, setWorking] = React.useState(false)
+
   const {showSb} = useSharedSnackbar()
 
-  const handleEnter = React.useCallback(async (value: string) => {
-    if (!value) {
+  const handleInputFH = React.useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setFh(e.target.value)
+  }, [])
+
+  const handleInputExtra = React.useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setExtra(e.target.value)
+  }, [])
+
+  const handleInputStill = React.useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setStill(e.target.checked)
+  }, [])
+
+  const handleSend = React.useCallback(async () => {
+    if (!fh) {
       showSb({open: true, severity: "warning", message: "番号为空"})
       return
     }
-    const resp = await request("/api/tgbot/fanhao/send", `fanhao=${value}`)
+
+    setWorking(true)
+    const data = `fanhao=${encodeURIComponent(fh)}&extra=${encodeURIComponent(extra)}&still=${still}`
+    const resp = await request("/api/tgbot/fanhao/send", data)
     const json: JResult<any> = await resp.json()
+
+    setWorking(false)
     if (json.code !== 0) {
       console.log(TAG, json.msg)
       showSb({open: true, severity: "error", message: json.msg})
       return
     }
 
-    showSb({open: true, severity: "success", message: `已成功记录番号 ${value}`})
-  }, [showSb])
+    showSb({open: true, severity: "success", message: `已成功记录番号 ${fh}`})
+    setFh("")
+    setExtra("")
+    setStill(false)
+  }, [fh, extra, still, showSb])
 
   return (
     <Stack direction={"column"} gap={1}>
       <Divider>记录番号</Divider>
-      <DoTextFieldBtn size={"small"} enterNode={"记录"} clearAfterEnter onEnter={handleEnter}/>
+      <TextField label={"番号"} required size={"small"} value={fh} onChange={handleInputFH}/>
+      <TextField label={"额外信息"} size={"small"} value={extra} onChange={handleInputExtra}/>
+      <FormControlLabel control={<Checkbox checked={still} onChange={handleInputStill}/>} label="重复发送"/>
+      <Button variant={"contained"} size={"small"} disabled={working} onClick={handleSend}>记录番号</Button>
     </Stack>
   )
 })
