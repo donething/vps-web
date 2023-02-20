f68488f4a3122d8d97d4c4d6eeb06a60fedecdfc
Donething <donething.net@gmail.com>
2023-02-20 17:07:07 +0800

    增加影片的标签

--- src/funcs/tgbot.tsx
+++ src/funcs/tgbot.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import {Button, Checkbox, Divider, FormControlLabel, Stack, TextField} from "@mui/material"
+import {Button, Checkbox, Divider, FormControlLabel, FormGroup, Stack, TextField} from "@mui/material"
 import {useSharedSnackbar} from "do-comps"
 import {request} from "do-utils"
 import {JResult} from "../comm/typedef"
@@ -8,16 +8,14 @@ import Auth from "../auth"
 // 标签
 const TAG = "[TGBot]"
 
-const label = "%23%E6%97%A0%E7%A0%81%20%23%E6%B5%81%E5%87%BA%20%23%E5%B7%A8%E4" +
-  "%B9%B3%20%23%E7%BE%8E%E5%B0%BB%20%23%E7%BE%8E%E8%84%9A%20%23%E5%8F%A3%E4%BA%A4" +
-  "%20%23%E6%8C%89%E6%91%A9"
-
 // 番号面板
 const FanHao = React.memo((): JSX.Element => {
-  const [fh, setFh] = React.useState("")
-  const [extra, setExtra] = React.useState("")
-  const [still, setStill] = React.useState(false)
   const [working, setWorking] = React.useState(false)
+  // 番号、额外信息、重复发送
+  const [fh, setFh] = React.useState("")
+  const [still, setStill] = React.useState(false)
+  // 已选择的标签
+  const [tagObj, setTagObj] = React.useState<{ [tag: string]: boolean }>({})
 
   const {showSb} = useSharedSnackbar()
 
@@ -25,13 +23,15 @@ const FanHao = React.memo((): JSX.Element => {
     setFh(e.target.value)
   }, [])
 
-  const handleInputExtra = React.useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    setExtra(e.target.value)
-  }, [])
-
   const handleInputStill = React.useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     setStill(e.target.checked)
   }, [])
+
+  const tags = React.useMemo(() => {
+    return Object.entries(tagObj).map(([tag, bool]) =>
+      <FormControlLabel key={tag} control={<Checkbox checked={bool} onChange={e =>
+        setTagObj(prev => ({...prev, [tag]: e.target.checked}))}/>} label={tag}/>)
+  }, [tagObj])
 
   const handleSend = React.useCallback(async () => {
     if (!fh) {
@@ -40,7 +40,8 @@ const FanHao = React.memo((): JSX.Element => {
     }
 
     setWorking(true)
-    const data = `fanhao=${encodeURIComponent(fh)}&extra=${encodeURIComponent(extra)}&still=${still}`
+    const tagsStr = Object.entries(tagObj).filter(([_, b]) => b).map(([tag]) => "#" + tag).join(" ")
+    const data = `fanhao=${encodeURIComponent(fh)}&tags=${encodeURIComponent(tagsStr)}&still=${still}`
     const resp = await request("/api/tgbot/fanhao/send", data)
     const json: JResult<any> = await resp.json()
 
@@ -52,19 +53,40 @@ const FanHao = React.memo((): JSX.Element => {
     }
 
     showSb({open: true, severity: "success", message: `已成功记录番号 ${fh}`})
+    // 恢复输入为初始值
     setFh("")
-    setExtra("")
     setStill(false)
-  }, [fh, extra, still, showSb])
+    setTagObj(prev => Object.fromEntries(
+      Object.entries(prev).map(([v]) => [[v], false])))
+  }, [fh, still, tagObj, showSb])
+
+  const init = React.useCallback(async () => {
+    const resp = await request("/api/tgbot/fanhao/tags")
+    const obj: JResult<string> = await resp.json()
+    if (obj.code !== 0) {
+      console.log(TAG, "获取影片标签错误：", obj.msg)
+      showSb({open: true, severity: "error", message: `获取影片标签错误：${obj.msg}`})
+      return
+    }
+
+    const tagsStr = decodeURIComponent(obj.data)
+    const tags = tagsStr.split(" ")
+    const map = Object.fromEntries(tags.map(v => [v, false]))
+    setTagObj(map)
+  }, [showSb])
+
+  React.useEffect(() => {
+    init()
+  }, [init])
 
   return (
     <Stack direction={"column"} gap={2}>
       <Divider>记录番号</Divider>
-      <TextField label={"番号"} required size={"small"} value={fh} onChange={handleInputFH}/>
-      <TextField label={`标签如 ${decodeURIComponent(label)}`} size={"small"} value={extra}
-                 onChange={handleInputExtra}/>
-      <FormControlLabel control={<Checkbox checked={still} onChange={handleInputStill}/>} label="重复发送"/>
-      <Button variant={"contained"} size={"small"} disabled={working} onClick={handleSend}>记录番号</Button>
+      <TextField label={"含番号的文本"} required size={"small"} value={fh} onChange={handleInputFH}/>
+      <FormGroup row sx={{gap: 1}}>{tags}</FormGroup>
+      <FormControlLabel sx={{width: "fit-content"}}
+                        control={<Checkbox checked={still} onChange={handleInputStill}/>} label="重复发送"/>
+      <Button variant={"contained"} disabled={working} onClick={handleSend}>记录番号</Button>
     </Stack>
   )
 })
